552bb46a79c64437467a4c4fc0de1e008b5bc68c
Mahendra_wijaya098 <72625796+mahendrawijaya123@users.noreply.github.com>
2022-07-07 18:38:20 +0700

    first commit

---
+++ uas_kas_rt/ci4/app/Views/template/admin_footer.php
@@ -0,0 +1,7 @@
+            <br>       
+            <footer>
+                <p>&copy; 2022 - Universitas Pelita Bangsa  |  Nama Mahasiswa : Mahendra wijaya  |  NIM : 312010249  |  Kelas : TI.20.A.2</p>
+            </footer>
+        </div>
+    </body>
+</html>
